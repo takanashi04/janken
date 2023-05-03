83050c8f2b3216957215da7e003b31a71c9b128b
EC2 Default User <ec2-user@ip-172-31-16-172.ap-northeast-1.compute.internal>
2023-05-03 07:59:07 +0000

    Ruby課題２提出

--- janken.rb
+++ janken.rb
@@ -1,38 +1,34 @@
 # じゃんけんフェーズの処理
 def janken
+  puts "じゃんけん・・・"
   puts "0がグー、1がチョキ、2がパー、3で逃げる"
   your_hand = gets.to_i
-  
-  # 例外の処理
-  if your_hand > 3
-    puts "０から3の間の数を入力してください"
-  end
   
   cpu_hand = rand(3)
   janken_hands = ["グー","チョキ","パー"]
   
-  puts "---------------------"
+    # あなたの3(逃げる)の時の処理
+  if your_hand == 3
+    puts "あなたは勝負から逃げました"
+    exit
+  end
+  
   puts "ポン！"
+  puts "---------------------"
   puts "あなたは#{janken_hands[your_hand]}を出しました"
   puts "相手は#{janken_hands[cpu_hand]}を出しました"
   puts "---------------------"
   
   if your_hand == cpu_hand
     puts "あいこ！"
-    return 0
+    return "aiko"
   elsif (your_hand == 0 && cpu_hand == 1) ||
         (your_hand == 1 && cpu_hand == 2)
     puts "あなたのターン！"
-    return 1
+    return "your_turn"
   else
     puts "相手のターン！"
-    return 2
-  end
-  
-  # あなたの3(逃げる)の時の処理
-  if your_hand == 3
-    puts "あなたは勝負から逃げました"
-    exit
+    return "cpu_turn"
   end
 end
   
@@ -44,14 +40,14 @@ def attimuite
   cpu_hoi = rand(3)
   
   hois = ["↑", "→", "↓", "←"]
-  puts "---------------------"
   puts "ホイ！"
+  puts "---------------------"
   puts "あなたは#{hois[your_hoi]}を出しました"
   puts "相手は#{hois[cpu_hoi]}を出しました"
   puts "---------------------"
   
   if your_hoi == cpu_hoi
-    puts "kimari"
+    puts "決まり！"
     return false
   else
     puts "もう一度！"
@@ -59,10 +55,26 @@ def attimuite
   end
 end
 
-if janken == 0
-  puts "あいこ"
-elsif janken == 1
-  puts "あなたのターン"
-else 
-  puts "相手のターン"
+next_game = true
+
+# 全体の処理
+while next_game
+  syouhai = janken
+  case syouhai
+  when "aiko"
+    next_game = true
+  when "your_turn"
+    next_game = attimuite
+    if next_game == true
+    else
+      puts "あなたの勝ち！！！"
+    end
+  when "cpu_turn"
+    next_game = attimuite
+    if next_game == true
+    else
+      puts "あなたの負け・・・"
+    end
+  else
+  end
 end
